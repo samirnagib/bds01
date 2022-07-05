f02ccb42a039e7e9e77e7d2539d5264abcf03fc1
Samir Nagib <samir.nagib@gmail.com>
2022-07-05 20:41:26 -0300

    Tests Passing

---
+++ src/main/java/com/devsuperior/bds01/repositories/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.devsuperior.bds01.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.devsuperior.bds01.entities.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
